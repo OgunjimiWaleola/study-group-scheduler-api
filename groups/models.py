a66098bceb78c93d5ffc6e00194e1b5a3dcdb3a9
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-02-17 22:33:39 +0100

    Create users and groups apps

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class StudyGroup(models.Model):
+    title = models.CharField(max_length=255)
+    subject = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    scheduled_date = models.DateTimeField()
+    max_participants = models.IntegerField(default=10)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="created_groups"
+    )
+    participants = models.ManyToManyField(
+        User, related_name="joined_groups", blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
