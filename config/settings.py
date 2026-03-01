5a50c9532f27e13c333190e2424f9c34a620d60d
Waleola Ogunjimi <waleolaogunjimi28@gmail.com>
2026-03-01 16:51:24 +0100

    Fix database configuration for PostgreSQL on Railway

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database 
 DATABASES = {
-    "default": dj_database_url.config(
+    'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
         ssl_require=True
